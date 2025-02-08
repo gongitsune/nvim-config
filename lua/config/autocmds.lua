d58138b7359da056e3152e27e805667ad95d238c
kara <89068705+gongitsune@users.noreply.github.com>
2025-02-08 11:29:05 +0900

    feat: switch main window at startup nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,6 +4,15 @@ local event = require("nui.utils.autocmd").event
 autocmd(event.BufEnter, {
   once = true,
   callback = function()
-    require("features.virtbuf"):open()
+    local virtbuf = require("features.virtbuf")
+    virtbuf:open()
+
+    -- Switch to the main window
+    vim.defer_fn(function()
+      local main_win = virtbuf:find_main_win()
+      if main_win then
+        vim.api.nvim_set_current_win(main_win)
+      end
+    end, 500)
   end,
 })
