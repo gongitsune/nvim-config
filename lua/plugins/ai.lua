0a8df1bcb29a0e13f9dd8c835388ddf176ff7ebb
kara <89068705+gongitsune@users.noreply.github.com>
2025-02-08 13:02:44 +0900

    feat: add copilot chat

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -15,4 +15,25 @@ return {
 			require("copilot").setup(opts)
 		end,
 	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" },                    -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken",                       -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		cmd = {
+			"CopilotChat",
+			"CopilotChatCommit",
+			"CopilotChatOpen",
+			"CopilotChatClose",
+			"CopilotChatToggle",
+			"CopilotChatStop",
+			"CopilotChatReset",
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 }
