d62121db2608588746411435af6269614dab7ac6
g22307 <g22307@kagawa.kosen-ac.jp>
2025-12-16 16:50:46 +0900

    feat: add json schema store

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -7,7 +7,10 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    ft = {"typescript", "typescriptreact"},
+    ft = { "typescript", "typescriptreact" },
     opts = {},
+  },
+  {
+    "b0o/schemastore.nvim",
   }
 }
