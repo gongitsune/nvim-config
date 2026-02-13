0135475217bb4090719d25b26174775528155cc9
g22307 <g22307@kagawa.kosen-ac.jp>
2026-02-13 23:34:55 +0900

    feat: update ai accept keymap

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,7 +7,7 @@ return {
       suggestion = {
         auto_trigger = true,
         keymap = {
-          accept = vim.fn.has("mac") == 0 and "<M-l>" or "¬"
+          accept = "<M-l>"
         }
       }
     }
